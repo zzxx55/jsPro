d5fa6825e08804e2ce2c7c8ee57912abedaaadc0
zzxx55 <87701940+zzxx55@users.noreply.github.com>
2022-01-23 14:32:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const products = [
+  { id: 1, title: 'Notebook', price: 1000 },
+  { id: 2, title: 'Mouse', price: 100 },
+  { id: 3, title: 'Keyboard', price: 250 },
+  { id: 4, title: 'Gamepad', price: 150 },
+];
+
+const renderProduct = (title, price, img = '') => {
+  return `<div class="product-item">
+                <h3>${title}</h3>
+                <p>${price}</p>
+                <button class="by-btn">Добавить</button>
+              </div>`;
+};
+
+const renderCatalog = (list) => {
+
+  const productBlock = document.querySelector('.products')
+  list.forEach(good => {
+    productBlock.insertAdjacentHTML('beforebegin', renderProduct(good.title, good.price));
+  });
+};
+
+renderCatalog(products);
+
+class Param {
+  constructor(products) {
+    this.id = products.id;
+    this.title = products.title;
+    this.price = products.prise;
+  }
+}
+
+class GoodsList {
+  constructor(id, title, price)
+  _sumPrice() {
+    let result = this.prise;
+    this.products.forEach(finalPrise => result += products.price);
+    return result;
+  }
+};
